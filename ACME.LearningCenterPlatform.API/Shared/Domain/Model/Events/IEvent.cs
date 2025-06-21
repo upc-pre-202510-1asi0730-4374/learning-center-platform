4d446c4af5452b24155cd06c575d5d7187ac27cd
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-06-20 22:57:37 -0500

    chore: add shared mediator pattern and configure on Program.cs

---
+++ ACME.LearningCenterPlatform.API/Shared/Domain/Model/Events/IEvent.cs
@@ -0,0 +1,8 @@
+using Cortex.Mediator.Notifications;
+
+namespace ACME.LearningCenterPlatform.API.Shared.Domain.Model.Events;
+
+public interface IEvent : INotification
+{
+    
+}
---
+++ ACME.LearningCenterPlatform.API/Shared/Infrastructure/Mediator/Cortex/Configuration/LoggingCommandBehavior.cs
@@ -0,0 +1,12 @@
+using Cortex.Mediator.Commands;
+
+namespace ACME.LearningCenterPlatform.API.Shared.Infrastructure.Mediator.Cortex.Configuration;
+
+public class LoggingCommandBehavior<TCommand>
+ : ICommandPipelineBehavior<TCommand> where TCommand : ICommand
+{
+ public async Task Handle(TCommand command, CommandHandlerDelegate next, CancellationToken cancellationToken)
+ {
+  await next();
+ }
+}
